d33c28fddbdb05c5880ff6e188b83af245aa5aab
yoshidan <naohiro.y@gmail.com>
2021-12-21 21:18:51 +0900

    start to add pubsub

--- googleapis/build.rs
+++ googleapis/build.rs
@@ -5,6 +5,7 @@ fn main() {
             .out_dir("src") // you can change the generated code's location
             .compile(
                 &[
+                    "googleapis/google/pubsub/v1/pubsub.proto",
                     "googleapis/google/spanner/v1/spanner.proto",
                     "googleapis/google/spanner/admin/database/v1/spanner_database_admin.proto",
                     "googleapis/google/spanner/admin/instance/v1/spanner_instance_admin.proto",
