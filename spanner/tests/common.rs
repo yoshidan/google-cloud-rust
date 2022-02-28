97608cd4fab8fcfe5f9a9f15335165ef55631168
yoshidan <naohiro.y@gmail.com>
2022-02-28 18:17:39 +0900

    update doc

--- spanner/tests/common.rs
+++ spanner/tests/common.rs
@@ -221,7 +221,7 @@ pub async fn read_only_transaction(session: ManagedSession) -> ReadOnlyTransacti
 pub async fn all_rows(mut itr: RowIterator<'_>) -> Vec<Row> {
     let mut rows = vec![];
     loop {
-        match itr.next().await {
+        match itr.next(None).await {
             Ok(row) => {
                 if row.is_some() {
                     rows.push(row.unwrap());
