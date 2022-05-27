65ebb5ef2aa4b9e952ac0aa19ec348ea1c401417
yoshidan <naohiro.y@gmail.com>
2022-05-27 12:40:13 +0900

    fmt

--- foundation/auth/src/token_source/mod.rs
+++ foundation/auth/src/token_source/mod.rs
@@ -7,8 +7,8 @@ use crate::error::Error;
 use crate::token::Token;
 use async_trait::async_trait;
 use serde::Deserialize;
-use std::time::Duration;
 use std::fmt::{Debug, Formatter};
+use std::time::Duration;
 
 #[async_trait]
 pub trait TokenSource: Send + Sync {
