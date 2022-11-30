ed4ec0fc90bdc21fe30dda2277e5b50763ed07a4
yoshidan <naohiro.y@gmail.com>
2022-11-30 17:29:15 +0900

    tweak

--- spanner-macro/tests/test.rs
+++ spanner-macro/tests/test.rs
@@ -1,11 +1,11 @@
 use chrono::{DateTime, NaiveDate, Utc};
 use google_cloud_spanner::client::Client;
+use google_cloud_spanner::mutation::insert_struct;
 use google_cloud_spanner::reader::AsyncIterator;
 use google_cloud_spanner::statement::Statement;
 use google_cloud_spanner::value::SpannerNumeric;
-use google_cloud_spanner_macro::{Table, Query};
+use google_cloud_spanner_macro::{Query, Table};
 use serial_test::serial;
-use google_cloud_spanner::mutation::insert_struct;
 
 #[derive(Table, Default)]
 pub struct UserCharacter {
@@ -27,9 +27,9 @@ pub struct UserItem {
 #[derive(Table, Default, Debug)]
 pub struct User {
     pub user_id: String,
-    #[spanner(name="NotNullINT64")]
+    #[spanner(name = "NotNullINT64")]
     pub not_null_int64: i64,
-    #[spanner(name="NullableINT64")]
+    #[spanner(name = "NullableINT64")]
     pub nullable_int64: Option<i64>,
     pub not_null_float64: f64,
     pub nullable_float64: Option<f64>,
@@ -51,7 +51,7 @@ pub struct User {
 }
 
 #[derive(Query)]
-pub struct UserBundle{
+pub struct UserBundle {
     pub user_id: String,
     pub user_characters: Vec<UserCharacter>,
     pub user_items: Vec<UserItem>,
@@ -59,7 +59,7 @@ pub struct UserBundle{
 
 #[tokio::test]
 #[serial]
-async fn test_table_derive() -> Result<(), anyhow::Error>{
+async fn test_table_derive() -> Result<(), anyhow::Error> {
     std::env::set_var("SPANNER_EMULATOR_HOST", "localhost:9010");
     let client = Client::new("projects/local-project/instances/test-instance/databases/local-database").await?;
 
@@ -77,11 +77,11 @@ async fn test_table_derive() -> Result<(), anyhow::Error>{
     stmt.add_param("UserID", &user_id);
     let mut reader = tx.query(stmt).await?;
     if let Some(row) = reader.next().await? {
-        let v : User = row.try_into()?;
+        let v: User = row.try_into()?;
         assert_eq!(v.user_id, user_id);
         assert_eq!(v.not_null_numeric.inner, "1".to_string());
         assert!(v.updated_at.timestamp() >= now);
-    }else {
+    } else {
         panic!("no data found");
     }
     Ok(())
@@ -89,7 +89,7 @@ async fn test_table_derive() -> Result<(), anyhow::Error>{
 
 #[tokio::test]
 #[serial]
-async fn test_query_derive() -> Result<(), anyhow::Error>{
+async fn test_query_derive() -> Result<(), anyhow::Error> {
     std::env::set_var("SPANNER_EMULATOR_HOST", "localhost:9010");
     let client = Client::new("projects/local-project/instances/test-instance/databases/local-database").await?;
 
@@ -107,31 +107,33 @@ async fn test_query_derive() -> Result<(), anyhow::Error>{
         user_id: user_id.clone(),
         ..Default::default()
     };
-    client.apply(vec![
-        insert_struct("User", user),
-        insert_struct("UserCharacter", user_character),
-        insert_struct("UserItem", user_item)
-    ]).await?;
+    client
+        .apply(vec![
+            insert_struct("User", user),
+            insert_struct("UserCharacter", user_character),
+            insert_struct("UserItem", user_item),
+        ])
+        .await?;
 
     let mut tx = client.read_only_transaction().await?;
-    let mut stmt = Statement::new("
+    let mut stmt = Statement::new(
+        "
     SELECT
         UserId,
 	    ARRAY(SELECT AS STRUCT * FROM UserCharacter WHERE UserId = @UserId) AS UserCharacters,
 	    ARRAY(SELECT AS STRUCT * FROM UserItem WHERE UserId = @UserId) AS UserItems,
     From User
-    WHERE UserID = @UserID"
+    WHERE UserID = @UserID",
     );
     stmt.add_param("UserID", &user_id);
     let mut reader = tx.query(stmt).await?;
     if let Some(row) = reader.next().await? {
-        let v : UserBundle = row.try_into()?;
+        let v: UserBundle = row.try_into()?;
         assert_eq!(v.user_id, user_id);
         assert_eq!(v.user_characters.len(), 1);
         assert_eq!(v.user_items.len(), 1);
-    }else {
+    } else {
         panic!("no data found");
     }
     Ok(())
-
 }
