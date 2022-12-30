5c85cdd352aa52e7cc665acb667dc58d0e952c93
Naohiro Yoshida <naohiro.y@gmail.com>
2022-12-30 12:09:42 +0900

    Revert "Use tokio 1.23 (#89)"
    This reverts commit 58a246fe82fd55c92fcdcf9723c0924f6f870c47.

--- storage/src/client.rs
+++ storage/src/client.rs
@@ -186,7 +186,7 @@ impl Client {
                 let der = pkcs
                     .to_pkcs8_der()
                     .map_err(|e| SignedURLError::CertError(e.to_string()))?;
-                let key_pair = ring::signature::RsaKeyPair::from_pkcs8(der.as_bytes())
+                let key_pair = ring::signature::RsaKeyPair::from_pkcs8(der.as_ref())
                     .map_err(|e| SignedURLError::CertError(e.to_string()))?;
                 let mut signed = vec![0; key_pair.public_modulus_len()];
                 key_pair
