4110af0efc5e9aa85887c93a6137ead16e9ce23e
yoshidan <naohiro.y@gmail.com>
2023-06-27 19:49:18 +0900

    fix error

--- bigquery/src/http/error.rs
+++ bigquery/src/http/error.rs
@@ -15,11 +15,6 @@ pub enum Error {
     TokenSource(Box<dyn std::error::Error + Send + Sync>),
 }
 
-/// An error response returned from Google Cloud Storage.
-///
-/// See the [`HTTP status and error codes for JSON`][1] documentation for more details.
-///
-/// [1]: https://cloud.google.com/storage/docs/json_api/v1/status-codes
 #[derive(Debug, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ErrorResponse {
@@ -27,9 +22,6 @@ pub struct ErrorResponse {
     ///
     /// Example values include: `400` (Bad Request), `401` (Unauthorized), and `404` (Not Found).
     pub code: u16,
-
-    /// A container for the error details.
-    pub errors: Vec<ErrorResponseItem>,
 
     /// Description of the error. Same as `errors.message`.
     pub message: String,
@@ -43,43 +35,6 @@ impl fmt::Display for ErrorResponse {
 
 impl std::error::Error for ErrorResponse {}
 
-#[derive(Debug, serde::Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct ErrorResponseItem {
-    /// The scope of the error. Example values include: `global` and `push`.
-    pub domain: String,
-
-    /// The specific item within the `locationType` that caused the error. For example, if you
-    /// specify an invalid value for a parameter, the `location` will be the name of the parameter.
-    ///
-    /// Example values include: `Authorization`, `project`, and `projection`.
-    pub location: Option<String>,
-
-    /// The location or part of the request that caused the error. Use with `location` to pinpoint
-    /// the error. For example, if you specify an invalid value for a parameter, the `locationType`
-    /// will be `parameter` and the `location` will be the name of the parameter.
-    ///
-    /// Example values include `header` and `parameter`.
-    pub location_type: Option<String>,
-
-    /// Description of the error.
-    ///
-    /// Example values include `Invalid argument`, `Login required`, and
-    /// `Required parameter: project`.
-    pub message: String,
-
-    /// Example values include `invalid`, `invalidParameter`, and `required`.
-    pub reason: String,
-}
-
-impl fmt::Display for ErrorResponseItem {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.message.fmt(f)
-    }
-}
-
-/// The GCS error response JSON format contains an extra object level that is inconvenient to include in our
-/// error.
 #[derive(serde::Deserialize)]
 pub(crate) struct ErrorWrapper {
     pub(crate) error: ErrorResponse,
