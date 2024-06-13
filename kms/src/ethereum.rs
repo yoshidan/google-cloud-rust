0c2bd6ebf56fc4070cafe532f6f20a2367369c01
yoshidan <naohiro.y@gmail.com>
2024-06-13 11:39:00 +0900

    pass test

--- kms/src/ethereum.rs
+++ kms/src/ethereum.rs
@@ -1,84 +1,44 @@
 use crate::ethereum::Error::InvalidSignature;
 use crate::grpc::apiv1::kms_client::Client as KmsGrpcClient;
-use asn1::{ParseError, ParseErrorKind, ParseResult, SimpleAsn1Readable, Tag};
 use google_cloud_gax::grpc::Status;
 use google_cloud_gax::retry::RetrySetting;
 use google_cloud_googleapis::cloud::kms::v1::{digest, AsymmetricSignRequest, Digest, GetPublicKeyRequest};
 use k256::ecdsa::{RecoveryId, Signature as ECSignature, VerifyingKey};
-use k256::elliptic_curve::bigint::Encoding;
-use k256::pkcs8::DecodePublicKey;
-use k256::U256;
-use k256::elliptic_curve::Curve;
+use k256::elliptic_curve::bigint::{CheckedSub, Encoding};
 use k256::elliptic_curve::sec1::ToEncodedPoint;
+use k256::elliptic_curve::Curve;
+use k256::pkcs8::DecodePublicKey;
+use std::ops::Div;
 use tiny_keccak::{Hasher, Keccak};
-
-struct U256Bridge<'a> {
-    value: U256,
-}
-
-impl<'a> U256Bridge<'a> {
-    pub fn as_bytes32(&self) -> [u8; 32] {
-        let mut b: Vec<u8> = vec![];
-        self.value.to_big_endian(&mut b);
-
-        while self.len() < 32 {
-            b.insert(0, 0);
-        }
-        b.into()
-    }
-}
-
-impl<'a> SimpleAsn1Readable<'a> for U256Bridge<'a> {
-    const TAG: Tag = Tag::primitive(0x02);
-    fn parse_data(data: &'a [u8]) -> ParseResult<Self> {
-        let value = U256::try_from(data).map_err(|_| ParseError::new(ParseErrorKind::InvalidValue))?;
-        Ok(Self { value })
-    }
-}
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error(transparent)]
     GRPC(#[from] Status),
     #[error(transparent)]
-    ParseError(#[from] asn1::ParseError),
+    K256ECError(#[from] k256::ecdsa::Error),
+    #[error(transparent)]
+    K256PKCSError(#[from] k256::pkcs8::spki::Error),
     #[error("invalid signature")]
     InvalidSignature(Vec<u8>),
 }
 
-pub struct PublicKey {
-    key: VerifyingKey,
-    address: [u8; 20],
-}
-
-impl TryFrom<VerifyingKey> for PublicKey {
-    type Error = ();
-
-    fn try_from(value: VerifyingKey) -> Result<Self, Self::Error> {
-        let point = value.as_affine().to_encoded_point(false);
-        let pubkey = point.to_bytes().try_into()?;
-        let address = keccak256(&pubkey[1..])[12..].try_into()?;
-        Ok(Self { key: value, address })
-    }
-}
-
+#[derive(Clone, Debug)]
 pub struct Signature {
-    value: [u8; 65],
-}
-
-impl AsRef<[u8; 65]> for Signature {
-    fn as_ref(&self) -> &[u8; 65] {
-        &self.value
-    }
+    pub r: [u8; 32],
+    pub s: [u8; 32],
+    pub v: u8,
 }
 
 impl Signature {
-    pub fn set_recovery_id(&mut self, recovery_id: u8) {
-        self.value[64] = recovery_id;
-    }
-
-    pub fn get_recovery_id(&self) -> u8 {
-        self.value[64]
+    pub fn to_bytes(&self) -> [u8; 65] {
+        let mut z = [0; 65];
+        let (r, rest) = z.split_at_mut(32);
+        let (s, v) = rest.split_at_mut(32);
+        r.copy_from_slice(&self.r);
+        s.copy_from_slice(&self.s);
+        v[0] = self.v;
+        z
     }
 }
 
@@ -91,42 +51,33 @@ impl<'a> EthereumSigner<'a> {
         Self { client }
     }
 
-    pub async fn get_address(&self, name: &str, option: Option<RetrySetting>) -> Result<[u8; 20], Error> {
+    pub async fn get_pubkey(&self, name: &str, option: Option<RetrySetting>) -> Result<VerifyingKey, Error> {
         let pubkey = self
             .client
             .get_public_key(GetPublicKeyRequest { name: name.to_string() }, option)
             .await?;
-        let pubkey = VerifyingKey::from_public_key_pem(&pubkey.pem)?;
-        key_to_address(pubkey)
+        Ok(VerifyingKey::from_public_key_pem(&pubkey.pem)?)
     }
 
     pub async fn sign(&self, name: &str, digest: &[u8], option: Option<RetrySetting>) -> Result<Signature, Error> {
         let request = asymmetric_sign_request(name, digest.to_vec());
         let result = self.client.asymmetric_sign(request, option.clone()).await?;
-
-        let mut signature = asn1::parse(result.signature.as_slice(), |d| {
-            return d.read_element::<asn1::Sequence>()?.parse(|d| {
-                let r = d.read_element::<U256Bridge>()?;
-                let s = d.read_element::<U256Bridge>()?;
-                Ok((r, s))
-            });
-        })?;
-
-        let (mut r, mut s) = signature;
-        if s.value < k256::Secp256k1::ORDER / 2 {
-            s.value = k256::Secp256k1::ORDER - s.value
+        let mut signature = ECSignature::from_der(&result.signature)?;
+        if let Some(new_sig) = signature.normalize_s() {
+            signature = new_sig
         }
-        let expected_address = self.get_address(name, option).await?;
+        let expected_key = self.get_pubkey(name, option).await?;
 
         for rid in 0..1 {
-            let sr = [s.as_bytes32(), r.as_bytes32()].concat();
-            let address = ecrecover(digest, sr.as_slice(), rid)?;
-            if expected_address != address {
-                continue;
+            let recovery_id = RecoveryId::from_byte(rid).unwrap();
+            let recovered_pubkey = VerifyingKey::recover_from_prehash(digest, &signature, recovery_id)?;
+            if recovered_pubkey == expected_key {
+                return Ok(Signature {
+                    r: signature.r().to_bytes().into(),
+                    s: signature.s().to_bytes().into(),
+                    v: rid,
+                });
             }
-            return Ok(Signature {
-                value: [sr, [rid]].concat().into(),
-            });
         }
         return Err(InvalidSignature(result.signature));
     }
@@ -143,20 +94,14 @@ fn asymmetric_sign_request(name: &str, digest: Vec<u8>) -> AsymmetricSignRequest
         data_crc32c: None,
     }
 }
-
-fn ecrecover(digest: &[u8], sr: &[u8], rid: u8) -> Result<[u8; 20], Error> {
-    let rid = RecoveryId::from_byte(rid)?;
-    let ec_signature = ECSignature::try_from(sr)?;
-    let pubkey = VerifyingKey::recover_from_prehash(digest, &ec_signature, rid)?;
-    key_to_address(pubkey)
-}
-
+/*
 fn key_to_address(value: VerifyingKey) -> Result<[u8; 20], Error> {
     let point = value.as_affine().to_encoded_point(false);
     let pubkey = point.to_bytes().try_into()?;
     let address = keccak256(&pubkey[1..])[12..].try_into()?;
     Ok(address)
 }
+ */
 
 fn keccak256(v: &[u8]) -> [u8; 32] {
     let mut k = Keccak::v256();
@@ -195,6 +140,8 @@ mod tests {
                 &hex!("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"),
                 None,
             )
-            .await;
+            .await
+            .unwrap();
+        print!("{:?}", result);
     }
 }
