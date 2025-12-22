172833927cc35b4b16f843dad65e4f1f246713a7
Nícolas Varela Auler <nicolasauler@usp.br>
2025-12-22 02:30:14 -0300

    feat: allow consumers of crate to choose their own crypto backend for jsonwebtoken (#416)

--- foundation/auth/src/lib.rs
+++ foundation/auth/src/lib.rs
@@ -5,3 +5,9 @@ mod misc;
 pub mod project;
 pub mod token;
 pub mod token_source;
+
+#[cfg(all(feature = "jwt-aws-lc-rs", feature = "jwt-rust-crypto"))]
+compile_error!("Enable only one feature: `jwt-aws-lc-rs` OR `jwt-rust-crypto`.");
+
+#[cfg(not(any(feature = "jwt-aws-lc-rs", feature = "jwt-rust-crypto")))]
+compile_error!("Enable one feature: `jwt-aws-lc-rs` OR `jwt-rust-crypto`.");
