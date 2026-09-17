0327959b8109a830e602c4fb7518678201bfe7fc
Tran Ngoc Tuan <94174684+tuantran0910@users.noreply.github.com>
2026-09-17 08:26:15 +0700

    feat(pubsub): make SchemaClient public so callers can reach the Schema API (#452)

--- pubsub/src/apiv1/schema_client.rs
+++ pubsub/src/apiv1/schema_client.rs
@@ -14,13 +14,12 @@ use google_cloud_googleapis::pubsub::v1::{
 use crate::apiv1::conn_pool::ConnectionManager;
 
 #[derive(Clone, Debug)]
-pub(crate) struct SchemaClient {
+pub struct SchemaClient {
     cm: Arc<ConnectionManager>,
 }
 
-#[allow(dead_code)]
 impl SchemaClient {
-    /// create new publisher client
+    /// create new schema client
     pub fn new(cm: ConnectionManager) -> SchemaClient {
         SchemaClient { cm: Arc::new(cm) }
     }
